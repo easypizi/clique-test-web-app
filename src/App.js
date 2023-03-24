a612d23d016e093a1d96e30270c560d4e7bf449d
Evan Tolstof <itolstof@gmail.com>
2023-03-24 17:28:33 +0700

    test1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
-// import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 // import UserList from './components/UserList/UserList';
 // import ChatList from './components/ChatList/ChatList';
-// import UserProfile from './components/UserProfile/UserProfile';
+import UserProfile from './components/UserProfile/UserProfile';
 import TabNavigation from './components/TabNavigation/TabNavigation';
 import { useTelegram } from './hooks/useTelegram';
 
@@ -21,11 +21,11 @@ function App() {
     <div className="App">
       <Header />
       <TabNavigation />
-      {/* <Routes> */}
-      {/* <Route index element={<UserList />} />
-        <Route path="/groups" element={<ChatList />} />
-        <Route path="/profile" element={<UserProfile />} /> */}
-      {/* </Routes> */}
+      <Routes>
+        {/* <Route index element={<UserList />} /> */}
+        {/* <Route path="/groups" element={<ChatList />} /> */}
+        <Route path="/profile" element={<UserProfile />} />
+      </Routes>
     </div>
   );
 }
