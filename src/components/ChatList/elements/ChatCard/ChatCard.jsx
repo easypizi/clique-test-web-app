d4293c5404cc82da1adbcff13410e4623d0ea7c0
Evan Tolstof <itolstof@gmail.com>
2023-04-05 19:40:12 +0700

    update chat list

--- src/components/ChatList/elements/ChatCard/ChatCard.jsx
+++ src/components/ChatList/elements/ChatCard/ChatCard.jsx
@@ -2,44 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
-import CampaignIcon from '@mui/icons-material/Campaign';
-import ChatIcon from '@mui/icons-material/Chat';
-import { Typography } from '@mui/material';
+import { green, pink, blue, orange } from '@mui/material/colors';
+import { Typography, Avatar, Box } from '@mui/material';
 
 const ChatItem = styled(Link)`
   display: flex;
   align-items: center;
   text-decoration: none;
   padding: 10px;
-  border-radius: 8px;
-  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-  transition: box-shadow 0.3s ease-in-out;
   color: inherit;
-
-  &:hover {
-    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);
-  }
 `;
 
 const IconWrapper = styled.div`
   margin-right: 10px;
 `;
 
+const color = () => {
+  const values = [green, pink, blue, orange];
+  const randomIndex = Math.floor(Math.random() * values.length);
+  return values[randomIndex];
+};
+
 function ChatCard({ groupName, groupLink, groupType = 'chat' }) {
-  const isChannel = groupType === 'channel';
+  const letters = `${groupName.split(' ')[0][0].toUpperCase()}${groupName
+    .split(' ')[1][0]
+    .toUpperCase()}`;
 
   return (
     <ChatItem to={groupLink}>
       <IconWrapper>
-        {isChannel ? (
-          <CampaignIcon sx={{ fontSize: 32 }} />
-        ) : (
-          <ChatIcon sx={{ fontSize: 32 }} />
-        )}
+        <Avatar sx={{ background: color()[500], width: 60, height: 60 }}>
+          {letters}
+        </Avatar>
       </IconWrapper>
-      <Typography variant="h6" sx={{ fontWeight: 500 }}>
-        {groupName}
-      </Typography>
+      <Box>
+        <Typography variant="h6" sx={{ fontWeight: 500 }}>
+          {groupName}
+        </Typography>
+        <Typography
+          color="GrayText"
+          variant="caption"
+          sx={{ marginTop: '3px' }}
+        >
+          {`TYPE: ${groupType}`}
+        </Typography>
+      </Box>
     </ChatItem>
   );
 }
