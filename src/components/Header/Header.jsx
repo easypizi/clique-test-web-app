98e4674688e7b48b3a8c9899dc2ffa4ba6f8a1cf
Evan Tolstof <itolstof@gmail.com>
2023-04-06 17:35:38 +0700

    fix header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { Box, Divider } from '@mui/material';
+import { Box, Fade } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import store from 'store2';
 
@@ -8,6 +8,11 @@ import LinkButton from '../LinkButton/LinkButton';
 
 function Header() {
   const { isUserDataLoading } = useSelector((state) => state.currentUser);
+
+  const isLoading = useMemo(
+    () => isUserDataLoading ?? false,
+    [isUserDataLoading]
+  );
 
   const storedUserId = store.session.get('userId');
   const storedPrivateId = store.session.get('privateId');
@@ -21,14 +26,25 @@ function Header() {
   );
 
   return (
-    !isUserDataLoading && (
-      <Box sx={{ width: '100%', margin: '5px 0 10px 0' }}>
-        <LinkButton variant="outlined" to={backToMainLink}>
-          <ArrowBackIcon />
-        </LinkButton>
-        <Divider sx={{ marginTop: '5px' }} />
-      </Box>
-    )
+    <Box
+      sx={{
+        width: '100%',
+        padding: '5px 0 10px 0',
+        height: '50px',
+        borderBottom: '0.5px solid lightgrey'
+      }}
+    >
+      <Fade
+        in={!isLoading}
+        style={{ transitionDelay: isLoading ? '0ms' : '300ms' }}
+      >
+        <div>
+          <LinkButton variant="outlined" to={backToMainLink}>
+            <ArrowBackIcon />
+          </LinkButton>
+        </div>
+      </Fade>
+    </Box>
   );
 }
 
