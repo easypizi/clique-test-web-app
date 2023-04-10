f65e6bcb5f4cdc959cffab6543f86029e73842dc
Evan Tolstof <itolstof@gmail.com>
2023-04-10 23:57:57 +0700

    add preventing modal window fucntion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-/* eslint-disable no-restricted-globals */
-/* eslint-disable no-alert */
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useTelegram } from './hooks/useTelegram';
 
@@ -15,9 +13,26 @@ function App() {
     tg.ready();
   }, [tg]);
 
-  window.onbeforeunload = function () {
-    return 'Что-нибудь сообщить пользователю';
-  };
+  // TODO: https://core.telegram.org/bots/webapps#events-available-for-web-apps
+  // Prevent accident closing of the window. Probably we should use height Change event and track this changes.
+  const thereAreUnsavedChanges = () => true;
+
+  const onBeforeUnload = useCallback((e) => {
+    if (thereAreUnsavedChanges()) {
+      e.preventDefault();
+      e.returnValue = '';
+      return;
+    }
+
+    delete e.returnValue;
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('beforeunload', onBeforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', onBeforeUnload);
+    };
+  }, [onBeforeUnload]);
 
   return (
     <div className="App">
