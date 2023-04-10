e8b0df4f86da66d7cf407214b3309767905fd9e0
Evan Tolstof <itolstof@gmail.com>
2023-04-10 23:34:15 +0700

    prevent closing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-alert */
 import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { useTelegram } from './hooks/useTelegram';
@@ -12,6 +14,19 @@ function App() {
   useEffect(() => {
     tg.ready();
   }, [tg]);
+
+  //
+
+  document.body.addEventListener('touchmove', (e) => {
+    if (e.touches[0].clientY < 50) {
+      // если свайп начинается вверху страницы
+      e.preventDefault(); // отменяем скролл
+      // показываем модальное окно с подтверждением
+      if (confirm('Вы уверены, что хотите закрыть приложение?')) {
+        tg.close();
+      }
+    }
+  });
 
   return (
     <div className="App">
